4b5f2fa43c7ea083595ec4e14b368d424603088c
Gustavo Rosolen Brigatti <gustavo.brigatti13@gmail.com>
2025-02-05 09:35:03 -0300

    first commit

---
+++ credentials/ContaAzulApi.credentials.ts
@@ -0,0 +1,36 @@
+import {
+	IAuthenticateGeneric,
+	ICredentialType,
+	INodeProperties,
+} from 'n8n-workflow';
+
+export class ContaAzulApi implements ICredentialType {
+	name = 'contaAzulApi';
+	displayName = 'Conta Azul API';
+	documentationUrl = 'https://docs.n8n.io/integrations/creating-nodes/build/declarative-style-node/';
+	properties: INodeProperties[] = [
+		{
+			displayName: 'Base URL',
+			name: 'baseUrl',
+			type: 'string',
+			default: '',
+			placeholder: 'https://example.com/api',
+			description: 'Base URL da API',
+		},
+		{
+			displayName: 'API Key',
+			name: 'apiKey',
+			type: 'string',
+			typeOptions: { password: true },
+			default: '',
+		}
+	];
+	authenticate: IAuthenticateGeneric = {
+		type: 'generic',
+		properties: {
+			headers: {
+				'x-access-key': '={{$credentials.apiKey}}',
+			},
+		},
+	};
+}
